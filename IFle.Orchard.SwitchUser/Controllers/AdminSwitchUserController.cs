0962b93ddb4690c346f017477662f6ff80d2e7a1
ifle <igor609@gmail.com>
2016-11-18 22:46:41 +0200

    init

---
+++ IFle.Orchard.SwitchUser/Controllers/AdminSwitchUserController.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Mvc;
+using IFle.Orchard.SwitchUser.Services;
+using Orchard;
+using Orchard.Environment.Extensions;
+using Orchard.Mvc.Extensions;
+using Orchard.Security;
+using Orchard.UI.Admin;
+using Orchard.Users.Events;
+
+namespace IFle.Orchard.SwitchUser.Controllers
+{
+    [OrchardFeature("IFle.Orchard.SwitchUser")]
+    [Admin]
+    public class AdminSwitchUserController : Controller
+    {
+        private readonly IMembershipService _membershipService;
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IUserEventHandler _userEventHandler;
+        private WorkContext _workContext;
+
+
+        public AdminSwitchUserController(
+            IAuthorizer authorizer,
+            IMembershipService membershipService,
+            IAuthenticationService authenticationService,
+            IUserEventHandler userEventHandler,
+            IWorkContextAccessor workContextAccessor)
+        {
+            _membershipService = membershipService;
+            _authenticationService = authenticationService;
+            _userEventHandler = userEventHandler;
+            _workContext = workContextAccessor.GetContext();
+        }
+        public ActionResult Index(string userName, string returnUrl = null)
+        {
+            if (!AdminSwitchUserFactory.IsSwitchUserAllowed(_workContext)) return new HttpUnauthorizedResult();
+
+            var user = _membershipService.GetUser(userName);
+
+            if (user == null) return HttpNotFound();
+
+            _authenticationService.SignIn(user, false);
+            _userEventHandler.LoggedIn(user);
+
+            returnUrl = "~/Admin/";
+            //if (string.IsNullOrEmpty(returnUrl)) returnUrl = "~/Admin/";
+
+            return this.RedirectLocal(returnUrl);
+        }
+    }
+}
---
+++ IFle.Orchard.SwitchUser/Services/AdminSwitchUserFactory.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Web.Routing;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.DisplayManagement;
+using Orchard.DisplayManagement.Implementation;
+using Orchard.Environment.Extensions;
+using Orchard.UI.Admin;
+using Orchard.Users.Models;
+
+namespace IFle.Orchard.SwitchUser.Services
+{
+    [OrchardFeature("IFle.Orchard.SwitchUser")]
+    public class AdminSwitchUserFactory : ShapeDisplayEvents
+    {
+        private readonly IContentManager _contentManager;
+        private readonly WorkContext _workContext;
+
+        public AdminSwitchUserFactory(
+            IWorkContextAccessor workContextAccessor,
+            IShapeFactory shapeFactory,
+            IContentManager contentManager)
+        {
+            _contentManager = contentManager;
+
+            _workContext = workContextAccessor.GetContext();
+            Shape = shapeFactory;
+        }
+
+        dynamic Shape { get; set; }
+
+        private bool IsActivable()
+        {
+            // activate on admin screen only
+            if (IsSwitchUserAllowed(_workContext))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether [is user selector allowed] [the specified work context].
+        /// </summary>
+        /// <param name="workContext">The work context.</param>
+        /// <returns>
+        ///   <c>true</c> if [is user selector allowed] [the specified work context]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsSwitchUserAllowed(WorkContext workContext)
+        {
+            var httpContext = workContext.HttpContext;
+            // TODO add settings for localhost
+            //return httpContext.Request.IsLocal && AdminFilter.IsApplied(new RequestContext(httpContext, new RouteData()));
+
+            return AdminFilter.IsApplied(new RequestContext(httpContext, new RouteData()));
+        }
+
+        public override void Displaying(ShapeDisplayingContext context)
+        {
+            context.ShapeMetadata.OnDisplaying(displayedContext =>
+            {
+                if (displayedContext.ShapeMetadata.Type == "Layout" && IsActivable())
+                {
+                    var userNameList = _contentManager.Query<UserPart, UserPartRecord>()
+                                        .OrderBy(userPart => userPart.UserName)
+                                        .List().Select(user => user.UserName).ToList();
+                    _workContext.Layout.Header.Add(Shape.AdminSwitchUser(Users: userNameList));
+                }
+            });
+        }
+    }
+}
